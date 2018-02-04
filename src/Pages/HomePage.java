da73729fc33c78cc7b28f6c9164c49ce6dff4e5b
Oguzcan <o.can.cosar@gmail.com>
2018-02-04 23:22:13 +0300

    added some elements

--- src/Pages/HomePage.java
+++ src/Pages/HomePage.java
@@ -28,6 +28,59 @@ public class HomePage extends BasePage {
 	@FindBy(css="button[id='search-icon-legacy']")
 	WebElement searchIcon;
 	
+	@FindBy(xpath = "")
+	WebElement searchBox;
+		
+	@FindBy(xpath = "")
+	WebElement signInButton;
+	
+	@FindBy(xpath = "")
+	WebElement homeLink;
+	
+	@FindBy(xpath = "")
+	WebElement trendingLink;
+	
+	@FindBy(xpath = "")
+	WebElement historyLink;
+	
+	@FindBy(xpath = "")
+	WebElement musicLink;
+	
+	@FindBy(xpath = "")
+	WebElement sportsLink;
+	
+	@FindBy(xpath = "")
+	WebElement 	gamingLink;
+	
+	@FindBy(xpath = "")
+	WebElement moviesLink;
+	
+	@FindBy(xpath = "")
+	WebElement newsLink;
+	
+	@FindBy(xpath = "")
+	WebElement threeHundredSixtyLink;
+	
+	@FindBy(xpath = "")
+	WebElement browseChannelsLink;
+	
+	@FindBy(xpath = "")
+	WebElement settingsLink;
+	
+	@FindBy(xpath = "")
+	WebElement helpLink;
+	
+	@FindBy(xpath = "")
+	WebElement sendFeedbackLink;
+	
+	@FindBy(xpath = "")
+	WebElement signInLink;
+	
+	
+	
+	
+	
+	
 	public void openHomePage(String URL) {
 		driver.get(URL);
 
